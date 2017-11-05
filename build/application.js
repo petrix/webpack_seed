bfcaba0682a7b3cb127ad90324e650a4d175cf6b
Ilya Lytvynov <ilyaillych@gmaile.com>
2017-11-05 12:29:25 +0200

    Added polyfills and file loader

---
+++ build/application.js
@@ -0,0 +1,32 @@
+webpackJsonp([0],{
+
+/***/ 125:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(89);
+module.exports = __webpack_require__(327);
+
+
+/***/ }),
+
+/***/ 327:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(328);
+
+var t = Array.from(['dasdas', 'asdasd']);
+console.log(t);
+
+/***/ }),
+
+/***/ 328:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[125]);
