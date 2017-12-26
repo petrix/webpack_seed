6a016effbb14e113f96085100fb7bc517ebf21f0
illial <illia.lytvynov@playtech.com>
2017-12-26 16:09:08 +0200

    Added empty project

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,4 @@
 import * as React from 'react';// eslint-disable-line
 import { render } from 'react-dom';
-import { DomainStore } from './stores/domain.store';
-import { AppRouter } from './Router';
-import './app.styl';
 
-const store = new DomainStore();
-
-render(<AppRouter/>, document.getElementById('root'));
+render(<h1>Hello world</h1>, document.getElementById('root'));
